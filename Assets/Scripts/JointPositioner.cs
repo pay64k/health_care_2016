98410fdd1a6f8b26da1576819ec10b63084557c9
jakub_desktop <pay64k@gmail.com>
2016-11-07 15:23:19 +0100

    main mechanics
    update for main game mechanics

--- Assets/Scripts/JointPositioner.cs
+++ Assets/Scripts/JointPositioner.cs
@@ -9,28 +9,52 @@ public class JointPositioner : MonoBehaviour {
 	public bool fixateX = true;
 	public bool fixateY = true;
 	public bool fixateZ = true;
-	
-	// Use this for initialization
-	void Start () {
+    public bool debugMode;
+
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    private Vector3 position;
+
+    // Use this for initialization
+    void Start () {
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		//Choose center
-		Vector3 center = Kinect.GetJointPos(KinectWrapper.Joints.SHOULDER_CENTER);
-		
-		//Release any non-fixated directions
-		if(!fixateX)
-			center.x = 0;
-		if(!fixateY)
-			center.y = 0;
-		if(!fixateZ)
-			center.z = 0;
-		
-		//Position this object according to chosen joint, but do it relative to the ShoulderCenter position
-		transform.localPosition = Kinect.GetJointPos(JointToTrack) - center;
+
+
+        if (!debugMode)
+        {
+            //Choose center
+            Vector3 center = Kinect.GetJointPos(KinectWrapper.Joints.SHOULDER_CENTER);
+
+            //Release any non-fixated directions
+            if (!fixateX)
+                center.x = 0;
+            if (!fixateY)
+                center.y = 0;
+            if (!fixateZ)
+                center.z = 0;
+            //Position this object according to chosen joint, but do it relative to the ShoulderCenter position
+            transform.localPosition = Kinect.GetJointPos(JointToTrack) - center;
+        }
+        else
+        {
+            position = Input.mousePosition;
+            position = new Vector3(
+                MapValue(position.x,0,1800,-20,20),
+                MapValue(position.y, 0, 1000, -15, 15),
+                0);
+            transform.localPosition = position;
+            
+        }
 	
 	}
+
+    public static float MapValue(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
 }
---
+++ Assets/Scripts/MainGameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainGameController : MonoBehaviour {
+
+    public PositionIndicatorSpawner IndicatorSpawner;
+    public float activeTimeSeconds = 1f;
+    public GameObject LeftHand;
+    public GameObject RightHand;
+
+
+    private float spawnTimer = 0f;
+    private bool indicatorsSpawned = false;
+    private GameObject indicator1;
+    private GameObject indicator2;
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!indicatorsSpawned)
+        {
+            indicator1 = IndicatorSpawner.SpawnIndicator(new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0));
+            indicator2 = IndicatorSpawner.SpawnIndicator(new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0));
+            indicatorsSpawned = !indicatorsSpawned;
+        }
+        spawnTimer += Time.deltaTime;
+        if(spawnTimer >= activeTimeSeconds)
+        {
+            Destroy(indicator1, 0);
+            Destroy(indicator2, 0);
+            spawnTimer = 0;
+            indicatorsSpawned = !indicatorsSpawned;
+        }
+
+    }
+}
