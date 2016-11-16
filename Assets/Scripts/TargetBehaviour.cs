8974b2a03aee72f54fcfbcca9c6f95b3ef361baa
pay64k <pay64k@gmail.com>
2016-11-16 16:30:57 +0100

    upd
    enemy movement

--- Assets/Scripts/TargetBehaviour.cs
+++ Assets/Scripts/TargetBehaviour.cs
@@ -6,13 +6,48 @@ public class TargetBehaviour : MonoBehaviour {
 
     public float health;
     public GameObject explosion;
+    public float swingRange;
 
+    public float max;
+    public float min;
+
+    private float swing;
+    private Vector3 originalPosition;
+    private float multi;
 
     void Start () {
         this.GetComponent<Rigidbody>().isKinematic = true;
+        swing = 1f;
+        originalPosition = transform.position;
+        //print("Orignial Position" + originalPosition.ToString());
+        multi = Random.Range(1, 3);
     }
 	
 	void Update () {
+        ////var swing = Random.RandomRange(-swingRange, swingRange);
+        //Vector3 oldPosition = transform.position;
+        ////print(oldPosition);
+        ////print(transform.position.x);
+        //if (oldPosition.x - swingRange <= 2f)
+        //{
+        //    transform.position = new Vector3(swing * Time.deltaTime, 0, 0) + oldPosition;
+        //    print("left");
+        //}
+        //else
+        //{
+        //    transform.position = new Vector3(-swing * Time.deltaTime, 0, 0) + oldPosition;
+        //    print("right");
+        //}
+        //transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, originalPosition.y, originalPosition.z);
+
+        var tempPosition = transform.position;
+        //max = Random.Range(3, 10);
+        //min = Random.Range(1, 2);
+        //min = 1f;
+        var newX = Mathf.PingPong(Time.time * multi, max - min) + min;// *Time.deltaTime;
+        //newX = newX * Time.deltaTime;
+        transform.position = new Vector3(newX + originalPosition.x, originalPosition.y, originalPosition.z);
+        
 	}
 
     void OnTriggerEnter(Collider other)
