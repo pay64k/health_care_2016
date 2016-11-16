e3c14aef22d74a94ce8d2de5495cc641330c07f6
pay64k <pay64k@gmail.com>
2016-11-16 14:44:05 +0100

    background added

---
+++ Assets/Scripts/scrollscript2.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrollscript2 : MonoBehaviour {
+	public float scrollSpeed;
+	private Vector2 savedOffset;
+	// Use this for initialization
+	void Start () {
+		savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset("_MainTex");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float y = Mathf.Repeat (Time.time * scrollSpeed, 1);
+		Vector2 offset = new Vector2 (savedOffset.x, y);
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);		
+		}
+
+		
+			
+				void OnDisable () {
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
+		}
+	}
+	
+	
+
