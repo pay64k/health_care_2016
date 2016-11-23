f9a92b66186b93119165d3943723404f42249061
jorrit27 <hii_hoi@live.nl>
2016-11-23 13:20:25 +0100

    Main menu

---
+++ Assets/Menu script.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Menuscript : MonoBehaviour {
+
+	public Canvas MainCanvas;
+
+	public void LoadOn()
+	{
+		SceneManager.LoadScene (1);
+	}
+
+
+
+}
---
+++ Assets/Menu2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Menu2 : MonoBehaviour {
+
+	public Canvas MainCanvas;
+
+	public void LoadOn()
+	{
+		SceneManager.LoadScene (1);
+	}
+
+
+
+}
